73da4093a2e8f45386a36eb403cc0709d2c76abc
Prateep Gedupudi <prateep.gedupudi@gmail.com>
2016-06-19 22:49:34 +0530

    Added euclidean distance function between points. Added closest function to find best label. Improved customised classification algorithm based on K Nearest Neighbour algorithm.

--- PipelineOwnClassifier.py
+++ PipelineOwnClassifier.py
@@ -1,14 +1,30 @@
-import random
+from scipy.spatial import distance
+def euc(a,b):
+    return distance.euclidean(a,b)
 class ScrappyKNN():
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
+
     def predict(self, X_test):
         predictions = []
         for row in X_test:
-            label = random.choice(self.y_train)
+            label = self.closest(row)
             predictions.append(label)
         return predictions
+
+    def closest(self,row):
+        best_distance = euc(row,X_train[0])
+        best_index = 0
+        for i in range(1, len(self.X_train)):
+            distance = euc(row, self.X_train[i])
+            if distance < best_distance:
+                best_distance = distance
+                best_index = i
+
+        return self.y_train[best_index]
+
+
 
 # import a data set
 from sklearn import datasets
